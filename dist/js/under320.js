990f0149503206152570cae5776bd2536932cd8a
Vadym Benedyk <o.oo.o@zoho.com>
2023-07-04 12:48:31 +0300

    err detected

--- dist/js/under320.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var root = document.getRootNode;
-root.classList.add("phone");
-document.body.appendChild(root);
-console.log(root);
--- src/js/under320.js
+++
@@ -1,5 +0,0 @@
-const root = document.getRootNode;
-
-root.classList.add("phone");
-document.body.appendChild(root);
-console.log(root);
